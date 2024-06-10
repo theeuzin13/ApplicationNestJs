3e3b7953a5858c943244ed6792f1d2011965d222
theeuzin13 <matt.henrique13700@gmail.com>
2024-06-10 16:58:13 -0300

    added hash in password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
     private users: User[] = [];
 
-
     async createUser(CreateUserDto: CreateUserDto): Promise<User> {
-        return ({
+        const saltOrRounds = 10;
+        const passwordHashed = await hash(CreateUserDto.password, saltOrRounds)
+
+
+        const user: User = {
             ...CreateUserDto,
-            id: 1,
-        })
+            id: this.users.length + 1,
+            password: passwordHashed
+        }
+
+        this.users.push(user)
+
+        return user
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        return this.users
     }
 
 }
